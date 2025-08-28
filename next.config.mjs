2e1aaf9e9f6e33a3afb3921cfa3b2f43a025ede2
TanishqSharma2022 <tanishq22@iiserb.ac.in>
2025-08-28 16:08:09 +0530

    tokens added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env:{
+        NEXT_PUBLIC_SANITY_PROJECT_ID: "gn9cx58k",
+        NEXT_PUBLIC_SANITY_DATASET: "production",
+    },
     typescript: {
         ignoreBuildErrors: true, // 🚨 Ignores all TS errors during build
     },
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
