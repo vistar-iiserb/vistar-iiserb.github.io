c0a74110974991c3851c2cfff2eadab6a06b913e
TanishqSharma2022 <tanishq22@iiserb.ac.in>
2025-08-28 16:42:17 +0530

    removed static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_SANITY_PROJECT_ID: "gn9cx58k",
-        NEXT_PUBLIC_SANITY_DATASET: "production",
-    },
+//     env: {
+//         NEXT_PUBLIC_SANITY_PROJECT_ID: "gn9cx58k",
+//         NEXT_PUBLIC_SANITY_DATASET: "production",
+//     },
 
-    async redirects() {
-    return [
-      {
-        source: '/studio/:path*',
-        destination: '/', // redirect Studio to homepage on GH Pages
-        permanent: false,
-      },
-    ];
-  },
+//     async redirects() {
+//     return [
+//       {
+//         source: '/studio/:path*',
+//         destination: '/', // redirect Studio to homepage on GH Pages
+//         permanent: false,
+//       },
+//     ];
+//   },
     typescript: {
         ignoreBuildErrors: true, // 🚨 Ignores all TS errors during build
     },
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    // output: "export",  // <=== enables static exports
+    // reactStrictMode: true,
 };
 
 export default nextConfig;
