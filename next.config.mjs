8a9478e94f14406411180b4188d41b4c090af4e2
TanishqSharma2022 <tanishq22@iiserb.ac.in>
2025-08-28 16:19:15 +0530

    excluding studio for github

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env:{
+    env: {
         NEXT_PUBLIC_SANITY_PROJECT_ID: "gn9cx58k",
         NEXT_PUBLIC_SANITY_DATASET: "production",
     },
+
+    async redirects() {
+    return [
+      {
+        source: '/studio/:path*',
+        destination: '/', // redirect Studio to homepage on GH Pages
+        permanent: false,
+      },
+    ];
+  },
     typescript: {
         ignoreBuildErrors: true, // 🚨 Ignores all TS errors during build
     },
