663460f1595f709af589f3ddb5cfcc05daf2a16f
TanishqSharma2022 <tanishq22@iiserb.ac.in>
2025-08-29 11:12:12 +0530

    typo fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // env: {
-    //     NEXT_PUBLIC_SANITY_PROJECT_ID: "gn9cx58k",
-    //     NEXT_PUBLIC_SANITY_DATASET: "production",
-    //     SANITY_API_READ_TOKEN:'ska2Uw8b0RXflMZYUvpfK6l6wC6QXkmZwxJArCixOcjmIvIrh4vUSGuXgJgj4WHk5N5j0g1VrxZLFt75gozWuezxYTs6nmOXgroQgIKQonkqBMwLJ3T6DYMrP8wkAknYhCp32yaBsbsGKHfp3nANfzDV6g8G8oPlvELkwyu7eeU9ePFX4rZ3'
-    // },
 
 //     async redirects() {
 //     return [
--- sanity/lib/token.ts
+++ sanity/lib/token.ts
@@ -1,7 +1,7 @@
 // src/sanity/lib/token.ts
 
-export const token = process.env.SANITY_API_READ_TOKEN;
+export const token = process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN;
 
 if (!token) {
-    throw new Error("Missing SANITY_API_READ_TOKEN");
+    throw new Error("Missing NEXT_PUBLIC_SANITY_API_READ_TOKEN");
 }
